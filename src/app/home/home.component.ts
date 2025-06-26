00681230c0fa1eb3c0f57a001bae94d92e4f3d13
karthi <karthi.athi@way.com>
2025-06-26 09:25:46 +0530

    Backend and Front end connected

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+@Injectable({ providedIn: 'root' })
 export class HomeComponent implements OnInit {
   showSpinner = true;
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  users = [];
   ngOnInit() {
     setTimeout(() => {
       this.showSpinner = false;
     }, 1000);
+   this.getUsers().subscribe((data: any) => {
+        this.users = data;
+        console.log(this.users);
+      });
+  }
+
+getUsers() {
+    return this.http.get(`${environment.apiUrl}/api/users`);
   }
 
 }
